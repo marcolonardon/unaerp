8f6a1da76c047eab0a7c9b6c2d3bb0bc6de02fb1
Marco A. Lonardon Jr <112179645+marcolonardon@users.noreply.github.com>
2023-02-06 21:54:51 -0300

    .

--- aula001/aula001.cpp
+++ aula001/aula001.cpp
@@ -1,6 +1,16 @@
-//cometários em uma linha
-/* comentários em
+//cometarios em uma linha
+/* comentarios em
 diversas linhas*/
 
-#include <stdio.h>//entrada e saída de dados
-#include <stdlib.h>//funçoes mais utilizadas
+#include <stdio.h>//entrada e saida de dados
+#include <stdlib.h>//funcoes mais utilizadas
+
+int soma;
+
+main(){
+	soma = 2 + 2;
+	printf("O valor de soma e: %i", soma);
+	return 0;
+}
+
+
