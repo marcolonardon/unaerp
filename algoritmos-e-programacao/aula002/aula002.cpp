00f1739991db391d605b2bf006773fc8cfccc8a7
0 <>
2023-02-13 21:14:52 -0300

    aula002

--- algoritmos-e-programacao/aula002/aula002.cpp
+++ algoritmos-e-programacao/aula002/aula002.cpp
@@ -1,1 +1,20 @@
- 
+//calcular mdeia final
+//M= pParcial * 0.4 + pFinal * 0.6
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float media, pParcial, pFinal;
+main(){
+	printf("Insira a nota da prova parcial: ");
+	scanf("%f", &pParcial);
+	printf("Insira a nota da prova final: ");
+	scanf("%f", &pFinal);
+	
+	media = pParcial * 0.4 + pFinal * 0.6;
+	
+	printf("\nA meida final e: %.1f\n\n", media);
+	
+	
+	system("pause");
+}
