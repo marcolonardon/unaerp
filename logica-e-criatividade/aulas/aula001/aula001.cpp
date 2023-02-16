76f09a6acf76c9d870f2f9999b86ecc5dd451dc5
Marco A. Lonardon Jr <112179645+StlleX@users.noreply.github.com>
2023-02-15 23:54:36 -0300

    semaforo-tinkercad

--- logica-e-criatividade/aulas/aula001/aula001.cpp
+++ logica-e-criatividade/aulas/aula001/aula001.cpp
@@ -1,21 +1,21 @@
 //semaforo arduino
 int ledVermelho=0, ledA=2, ledVerde=1; 
 void setup(){
- pinMode(ledVermelho, OUTPUT);
- pinMode(ledVerde, OUTPUT);
- pinMode(ledA, OUTPUT); 
+    pinMode(ledVermelho, OUTPUT);
+    pinMode(ledVerde, OUTPUT);
+    pinMode(ledA, OUTPUT); 
 }
 
 void loop(){
- digitalWrite(ledVermelho, HIGH); 
- delay(2000); 
- digitalWrite(ledVermelho, LOW); 
-  
- digitalWrite(ledA, HIGH);
- delay(1000);
- digitalWrite(ledA, LOW); 
- 
- digitalWrite(ledVerde, HIGH); 
- delay(2000);
- digitalWrite(ledVerde, LOW); 
+    digitalWrite(ledVermelho, HIGH); 
+    delay(2000); 
+    digitalWrite(ledVermelho, LOW); 
+    
+    digitalWrite(ledA, HIGH);
+    delay(1000);
+    digitalWrite(ledA, LOW); 
+    
+    digitalWrite(ledVerde, HIGH); 
+    delay(2000);
+    digitalWrite(ledVerde, LOW); 
 }
