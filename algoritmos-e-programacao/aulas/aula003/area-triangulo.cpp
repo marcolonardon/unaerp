64e55ddb3270bf492eba9612f1faca4c0baf55df
Marco A. Lonardon Jr <112179645+marcolonardon@users.noreply.github.com>
2023-02-27 22:15:53 -0300

    area-triangulo

---
+++ algoritmos-e-programacao/aulas/aula003/area-triangulo.cpp
@@ -0,0 +1,29 @@
+/* Area do triangulo: algoritmo para ler a base e a altura de um triangulo e
+imprimir a base, altura e area.
+verificar se abase e/ou a altura e negativa. Caso afirmativo, impriir mensagem de erro e nao calcular a area.
+Caso a base e a altura sejam ambas negativas, imprimir mensagem e nao calcular a area*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+float base, h, area;
+
+main(){
+	printf("Insira o valor da base do triangulo: ");
+	scanf("%f", &base);
+	printf("\nInsira o valor da altura do triangulo: ");
+	scanf("%f", &h);
+	
+	if (base < 0 && h < 0){
+		printf("\nBase e altura negativas, nao e possivel calcular a area.\n");
+	}else if(base < 0 && h > 0){
+		printf("\nBase negativa, nao e possivel calcular a area.\n");
+	}else if(base > 0 && h < 0){
+		printf("\nAltura negativa, nao e possivel calcular a area.\n");
+	}else{
+		area = (base*h)/2;
+		printf("\nA area de um triangulo de base: %.2f e altura: %.2f eh %.2f\n", base, h, area);
+	}
+	
+	system("pause");
+}
