21b32afbd509583e6815391a9ec6ce88c7fc218b
Marco A. Lonardon Jr <112179645+marcolonardon@users.noreply.github.com>
2023-02-27 21:42:10 -0300

    aula003

--- algoritmos-e-programacao/aulas/aula003/salario-familia.cpp
+++ algoritmos-e-programacao/aulas/aula003/salario-familia.cpp
@@ -1,6 +1,6 @@
 /*calcular o valor do salario familia que um colaborador devera receber;
- para o pagamento do salario familia tem-se :
- quem recebe salario ate R$ 806,80 recebera 41,37 por filho e quem recebe acima de 806,8 recebera 29,16 por filho.
+ para o pagamento do salario familia tem-se:
+ quem recebe salario ate R$ 806,80 recebera R$ 41,37 por filho e quem recebe acima de R$ 806,8 recebera R$ 29,16 por filho.
  o algoritmo devera ler o salario do colaborador, o numero de filhos e imprimir o salario familia*/
  
  #include <stdio.h>
@@ -9,7 +9,7 @@
  int filhos;
  float salario, sfamilia;
  main(){
- 	printf("\nInsira seu salario mensal: ");
+ 	printf("Insira seu salario mensal: ");
  	scanf("%f", &salario);
  	printf("\nInsira a quantidade de filhos: " );
  	scanf("%i", &filhos);
@@ -19,8 +19,8 @@
 		printf("\nSalario familia: R$ %.2f\n", sfamilia);		
 	}else{
 		sfamilia = salario + filhos*29.16;
-		printf("Salario familia: R$ %.2f\n", sfamilia); 	
-	 }
+		printf("\nSalario familia: R$ %.2f\n", sfamilia); 	
+	}
 	 
  	system("pause");
  }
