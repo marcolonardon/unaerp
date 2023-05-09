e7d3c567bf027cb9dce10d74a33ce48afc56b0ca
Marco A. Lonardon Jr <112179645+marcolonardon@users.noreply.github.com>
2023-05-08 23:34:02 -0300

    aula010

--- algoritmos-e-programacao/aulas/aula010/suavizacao.cpp
+++ algoritmos-e-programacao/aulas/aula010/suavizacao.cpp
@@ -5,26 +5,28 @@
 
 float vetor[5], vetor_suavizado[5];
 
-main(){
-	for(int i=0; i<5; i++){
-		printf("Informe o valor %i: ", i+1);
+main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		printf("Informe o valor %i: ", i + 1);
 		scanf("%f", &vetor[i]);
-		
 	}
-	
+
 	vetor_suavizado[0] = vetor[0];
-	vetor_suavizado[4] = vetor[4];	
-	
-	for(int i=1; i<4; i++){
-		vetor_suavizado[i] = (vetor[i-1]+vetor[i]+vetor[i+1]) / 3;
-		
+	vetor_suavizado[4] = vetor[4];
+
+	for (int i = 1; i < 4; i++)
+	{
+		vetor_suavizado[i] = (vetor[i - 1] + vetor[i] + vetor[i + 1]) / 3;
 	}
-	
-	for(int i=0; i<5; i++){
+
+	for (int i = 0; i < 5; i++)
+	{
 		printf("%.2f  ", vetor[i]);
-		printf("\n");
-		printf("%.2f  ", vetor_suavizado[i]);
+		printf(" %.2f  ", vetor_suavizado[i]);
+		printf("\n\n");
 	}
-	
+
 	system("pause");
 }
