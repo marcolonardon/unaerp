c0d714ad30fce345d827713fdf5fffe96ec837cc
Marco A. Lonardon Jr <112179645+marcolonardon@users.noreply.github.com>
2023-11-21 21:55:51 -0300

    dia21

--- tecnicas-de-programacao/criarArquivo/ex-cadastro2-prova/criar-csv.c
+++ tecnicas-de-programacao/criarArquivo/ex-cadastro2-prova/criar-csv.c
@@ -1,48 +1,11 @@
-/*ATIVIDADE: Escreva um software que leia para um vetor de struct
-em memória o RA, etapa em que se encontra, ano de inicio, valor
-da mensalidade, bolsa se houver e e-mails de contato. 
+/*Marco AntÃ´nio Lonardon JÃºnior - 839462*/
 
-
-
-
-O programa deve conter no menu: 1 – cadastrar novo; 
-								2- listar cadastro ; 
-								3 - gerar csv; 
-								4 – sair; 
-								
-onde, o item 1 preenche o vetor um estudante
-por vez; 
-o item 2 exibe o vetor em forma tabelada;
- o tem 3 pede o
-nome do arquivo e salva o conteúdo do vetor em um arquivo .csv,
-com campos separados por “;” 
-e finalmente o item 4 encerra o programa.
-
-
-* Corpo da função principal deve ser:
-int main (){ menu(parâmetros); } onde antes da chamada de
-menu é permitida a declaração da dados e estruturas de dados
-necessárias. (qualquer outra modificação,
-* Bibliotecas autorizadas: stdio.h e stdlib.h (adição de
-outras modifica o escopo)
-
-COMO ENTREGAR:
-
-* Nome e RA do estudante deve constar no início do arquivo-fonte
-principal (que contém a função main)em forma de comentário.
-(redução de 10% da nota)
-* Entregar exclusivamente pelo classroom, os arquivos originais.
-Links não serão aceitos.
-* Não compactar. (redução de 1/3 da nota).*/
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <string.h>
 
 #define MAX_ALUNOS 30
 #define MAX_EMAIL 25
-
-int flag=1;
 
 struct dados_alunos {
 /*RA, etapa em que se encontra, ano de inicio, valor da mensalidade, bolsa se houver e e-mails de contato.*/
@@ -50,10 +13,11 @@ struct dados_alunos {
 	int codigo;
 	int etapa;
 	int ano_inicio;
-	int valor_mensalidade;
-	int valor_bolsa;
+	float valor_mensalidade;
+	float valor_bolsa;
     char email[MAX_EMAIL];
 };
+struct dados_alunos novoAluno;
 
 void cadastrar_aluno(struct dados_alunos alunos[], int *total_alunos);
 void exibir_alunos(struct dados_alunos alunos[], int total_alunos);
@@ -68,7 +32,6 @@ void cadastrar_aluno(struct dados_alunos alunos[], int *total_alunos) {
         return;
     }
 
-    struct dados_alunos novoAluno;
     
     /*codigo, etapa , ano_inicio, valor_mensalidade, valor_bolsa, e-mail*/
     printf("Codigo: ");
@@ -81,10 +44,10 @@ void cadastrar_aluno(struct dados_alunos alunos[], int *total_alunos) {
     scanf("%i", &novoAluno.ano_inicio);
     
     printf("Valor da mensalidade: ");
-    scanf("%i", &novoAluno.valor_mensalidade);
+    scanf("%f", &novoAluno.valor_mensalidade);
     
     printf("Valor da bolsa (se nao houver, insira o valor 0): ");
-    scanf("%i", &novoAluno.valor_bolsa);
+    scanf("%f", &novoAluno.valor_bolsa);
     
     printf("E-Mail: ");
     scanf("%s", novoAluno.email);
@@ -101,44 +64,39 @@ void cadastrar_aluno(struct dados_alunos alunos[], int *total_alunos) {
     alunos[*total_alunos] = novoAluno;
     (*total_alunos)++;
 
-    salvarEmArquivo(novoAluno);
+    //salvarEmArquivo(novoAluno);
 }
 
 void exibir_alunos(struct dados_alunos alunos[], int total_alunos){
-    printf("\nAlunos cadastrados:\n");
 /*codigo, etapa , ano_inicio, valor_mensalidade, valor_bolsa, e-mail*/
 	printf("CODIGO\tETAPA\tANO\tMENSALIDADE\tBOLSA\tEMAIL\t\n\n");
 
     for(int i = 0; i < total_alunos; i++){
-        printf("%i\t%i\t%i\t%.2f\t%.2f\t%s\t\n", alunos[i].codigo,alunos[i].etapa, alunos[i].ano_inicio,
+        printf("%i\t%i\t%i\t%.2f\t\t%.2f\t%s\t\n", alunos[i].codigo,alunos[i].etapa, alunos[i].ano_inicio,
                alunos[i].valor_mensalidade, alunos[i].valor_bolsa, alunos[i].email);
     }
 }
 
 void salvarEmArquivo(struct dados_alunos aluno) {
+	char nome_arquivo[25];
+	printf("\nInforme o nome do arquivo: ");
+	scanf("%s", &nome_arquivo);
+	
+	strcat(nome_arquivo, ".csv");
+	
     FILE *arquivo;
 
-    arquivo = fopen("cadastro.txt", "a");
+    arquivo = fopen(nome_arquivo, "w");
 
     if(arquivo == NULL){
         printf("Erro!!!\n");
         return;
     }
 	
-	if(flag==1){
-		 /*codigo, etapa , ano_inicio, valor_mensalidade, valor_bolsa, e-mail*/
-		fprintf(arquivo, "CODIGO\tETAPA\tANO\tMENSALIDADE\tBOLSA\tEMAIL\t\n\n");
-		flag++;
-	}
-	
-    fprintf(arquivo, "%i\t%i\t%i\t%.2f\t%.2f\t%s\t\n", aluno.codigo, aluno.etapa,
+    fprintf(arquivo, "%i;%i;%i;%.2f;%.2f;%s;\n", aluno.codigo, aluno.etapa,
 	aluno.ano_inicio, aluno.valor_mensalidade, aluno.valor_bolsa, aluno.email);
 
     fclose(arquivo);
-}
-
-float calcular_media(float parcial, float exame){
-    return (parcial*0.4)+(exame*0.6);
 }
 
 void menu(){
@@ -159,13 +117,17 @@ void menu(){
                 exibir_alunos(alunos, total_alunos);
                 break;
             case 3:
+            	salvarEmArquivo(novoAluno);
+                printf("Arquivo gerado.\n");
+                break;
+            case 4:
                 printf("Programa finalizado.\n");
                 break;
             default:
                 printf("opcao invalida.\n");
                 break;
         }
-    }while (op != 3);
+    }while (op != 4);
 
 }
 
