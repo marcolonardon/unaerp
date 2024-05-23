68433f2fc01891b3afbe3cca43d82c4904493d0c
Marco A. Lonardon Jr. <112179645+marcolonardon@users.noreply.github.com>
2024-05-22 21:53:46 -0300

    TesteHash

--- estrutura-de-dados/hash-function/testeHashRPG.cpp
+++ estrutura-de-dados/hash-function/testeHashRPG.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
+// precisa adicionar uma verificacao para nomes iguais
 int hashFunction(const string& nome){
 	int hashValue = 0;
-	for(char c : nome){
-		if(c >= '0' && c <= 'z'){
-			hashValue += c;
+	for(char c : nome){ // itera por cada char do nome
+		if(c >= '0' && c <= 'z'){ // se for dentro desse parâmetro da ASCII (Todas as letras e números - e alguns chars especiais)
+			hashValue += c; // faz a soma de cada valor da ascii para cada char
 		}
 	}
 	
