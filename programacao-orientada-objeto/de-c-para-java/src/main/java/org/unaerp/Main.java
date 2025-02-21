7852152bf679305bd6fd7614c71a8186a69548dc
Marco A. Lonardon Jr. <112179645+marcolonardon@users.noreply.github.com>
2025-02-20 22:04:53 -0300

    commit

--- programacao-orientada-objeto/de-c-para-java/src/main/java/org/unaerp/Main.java
+++ programacao-orientada-objeto/de-c-para-java/src/main/java/org/unaerp/Main.java
@@ -1,9 +1,25 @@
 package org.unaerp;
 
+//public class Main {
+//    public static void main(String[] args){
+//        Pessoa pessoa = new Pessoa("Nome", 2, 2, 2000);
+//
+//        System.out.println("Idade = "+ pessoa.calculaIdade(18, 2, 2025));
+//    }
+//}
+
 public class Main {
     public static void main(String[] args){
-        Pessoa pessoa = new Pessoa("Nome", 2, 2, 2000);
+        Conta bruno = new Conta(1, "Bruno");
+        Conta jose = new Conta (2, "Jose");
 
-        System.out.println("Idade = "+ pessoa.calculaIdade(18, 2, 2025));
+        bruno.depositar(1200);
+        jose.depositar(500);
+        bruno.sacar(200);
+        jose.depositar(300);
+        bruno.transferir(jose, 100);
+
+        System.out.println("Saldo Bruno "+bruno.getSaldo());
+        System.out.println("Saldo Jose "+jose.getSaldo());
     }
 }
